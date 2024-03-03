626bfdca93c4e9ba0354d61346a15b22c94cce8b
Andrzej <andrzejkar17@gmail.com>
2024-03-03 10:44:32 +0100

    Create 'categories' table migration and schema
    This commit introduces a new migration for creating a 'categories' table and an attribute 'name'. Additionally, the schema.rb file has been updated to incorporate these changes, advancing the version timestamp.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_150014) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_094345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"

6b5cc9daf53ced34eb62934125cb291d319d5aa7
Andrzej <andrzejkar17@gmail.com>
2024-03-14 18:05:10 +0100

    Add CategoryType in GraphQL
    Created a new CategoryType in GraphQL including id, name, created_at and updated_at fields. This type is designed to handle category-related operations in our GraphQL schema.

---
+++ app/graphql/types/category_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class CategoryType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end

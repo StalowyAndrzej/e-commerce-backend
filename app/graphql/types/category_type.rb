f0d4afbc49f74b667376377ea9e6b7a8a9a6b01c
Andrzej <andrzejkar17@gmail.com>
2024-03-14 18:06:08 +0100

    Add immutability directive and restructure fields in category_type.rb
    Set 'frozen_string_literal: true' to enforce string immutability in category_type.rb. The fields in the CategoryType class were also rearranged for better readability, positioning the 'created_at' field at the top for logical order.

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module Types
   class CategoryType < Types::BaseObject
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
     field :name, String, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end

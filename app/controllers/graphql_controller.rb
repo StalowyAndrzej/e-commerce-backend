5cbc7a8fb0e806e27466b9428934e8547b60eb9e
Andrzej <andrzejkar17@gmail.com>
2024-03-14 18:21:19 +0100

    Refactor GraphqlController's backend execution and error handling
    Added 'frozen_string_literal: true' at the top of the file to maintain immutability for the strings used. Also, updated the backend execution of the query and variables to be more precise.

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
@@ -12,10 +14,12 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = ECommerceBackendSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = ECommerceBackendSchema.execute(query, variables:, context:,
+                                                   operation_name:)
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development(e)
   end
 
@@ -45,6 +49,6 @@ class GraphqlController < ApplicationController
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: :internal_server_error
   end
 end

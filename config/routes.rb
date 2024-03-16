28b741a39ade86a5699b24a0ac238340109a9703
Andrzej <andrzejkar17@gmail.com>
2024-03-16 15:46:57 +0100

    Update GraphQL schema and adjust routes
    This commit modifies parameters in several methods of the e_commerce_backend_schema to improve clarity and readability. Additionally, it refines the mounting of GraphiQL::Rails::Engine in the routes file to only occur in the development environment.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 end

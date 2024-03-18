10aa3fabaf24bee430a2f16663b7ba6eafe69a63
Andrzej <andrzejkar17@gmail.com>
2024-03-18 09:25:11 +0100

    Add 'frozen_string_literal: true' to GraphQL types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
@@ -9,9 +11,9 @@ module Types
 
     # TODO: remove me
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
+                               description: 'An example field added by the generator'
     def test_field
-      "Hello World!"
+      'Hello World!'
     end
   end
 end

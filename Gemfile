d1e940bb561967c25b66f233ce06ec5f893ff8e6
Andrzej <andrzejkar17@gmail.com>
2024-03-19 10:13:35 +0100

    Add 'faker' gem and implement in seeds.rb
    The 'faker' gem has been added to the Gemfile, bundled in Gemfile.lock, and implemented in seeds.rb in order to generate sample data for Products. This provides a more realistic environment for testing and development.

--- Gemfile
+++ Gemfile
@@ -28,7 +28,9 @@ gem 'bootsnap', require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
 # gem "rack-cors"
 
-gem "sprockets-rails"
+gem 'sprockets-rails'
+
+gem 'faker'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+20.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence(word_count: 3),
+    price: Faker::Commerce.price(range: 10..100.0),
+    image: Faker::LoremFlickr.image(size: '200x300', search_terms: ['product'])
+  )
+end

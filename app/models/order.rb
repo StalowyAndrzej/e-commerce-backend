eb29dcf0530f7151c867141150cbfac6123a298c
Andrzej <andrzejkar17@gmail.com>
2024-04-01 10:31:24 +0200

    Remove order models

--- app/models/order.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Order < ApplicationRecord
-  has_many :order_items
-  enum status: { pending: 0, processing: 1, completed: 2, cancelled: 3 }
-end
--- app/models/order_item.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class OrderItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :order
-end

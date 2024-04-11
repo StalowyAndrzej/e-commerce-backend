dcf7cdf9659461dc44d01f0606acc513f8a82e88
Andrzej <andrzejkar17@gmail.com>
2024-04-11 07:50:17 +0200

    Add product seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price: Faker::Commerce.price(range: 10..100),
+    image: Faker::LoremFlickr.image(size: "300x300"),
+    created_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now),
+    updated_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now)
+  )
+end
+
+puts "Seed data has been successfully added!"

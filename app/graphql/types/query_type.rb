70e84438a3fbe708d35193729d09ae7a337fce74
Andrzej <andrzejkar17@gmail.com>
2024-04-14 11:16:13 +0200

    Provide nodes in query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,24 +2,26 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :products, String, null: false
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-
-    def products
-      Product.all
+    field :node, Types::NodeType, null: true, description: "Fetches an
+  object given its ID." do
+      argument :id, ID, required: true, description: 'ID of the object.'
     end
 
-    def test_field
-      'Hello World!'
+    field :nodes, [Types::NodeType, { null: true }], null: true, description:
+      'Fetches a list of objects given a list of IDs.' do
+      argument :ids, [ID], required: true, description: "IDs of the
+  objects."
+    end
+
+    def node(id:)
+      context.schema.object_from_id(id, context)
+    end
+
+    def nodes(ids:)
+      ids.map { |id| context.schema.object_from_id(id, context) }
     end
   end
 end

74610c2c3cf27a6d91ea5109336ffa1f4fd3875b
Andrzej <andrzejkar17@gmail.com>
2024-04-22 07:53:23 +0200

    Schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_25_072441) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_21_082757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"

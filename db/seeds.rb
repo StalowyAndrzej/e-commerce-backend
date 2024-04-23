a53f8e983754029a87132fa45694fa8ee78213d1
Andrzej <andrzejkar17@gmail.com>
2024-04-23 07:49:09 +0200

    Add Category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraph(sentence_count: 2),
     price: Faker::Commerce.price(range: 10..100),
-    image: Faker::LoremFlickr.image(size: "300x300"),
+    image: Faker::LoremFlickr.image(size: '300x300'),
     created_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now),
     updated_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now)
   )
 end
 
-puts "Seed data has been successfully added!"
+10.times do
+  Category.create(
+    name: Faker::Commerce.department(max: 2, fixed_amount: true),
+    description: Faker::Lorem.sentence,
+    is_active: Faker::Boolean.boolean(true_ratio: 0.8)
+  )
+end
+
+Rails.logger.debug 'Seed data has been successfully added!'

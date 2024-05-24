bf89b3f3b8f51c12368e82e88cee52ec1f9c94e2
Andrzej <andrzejkar17@gmail.com>
2024-05-24 07:24:26 +0200

    Lint config env files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
@@ -45,7 +47,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new($stdout)
                                        .tap  { |logger| logger.formatter = Logger::Formatter.new }
                                        .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 

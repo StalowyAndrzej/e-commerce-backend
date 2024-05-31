e1589015cfca0e5cfef813a4e6615575e0cad444
Andrzej <andrzejkar17@gmail.com>
2024-05-31 07:49:31 +0200

    Remove models

--- app/graphql/e_commerce_backend_schema.rb
+++ app/graphql/e_commerce_backend_schema.rb
@@ -8,13 +8,6 @@ class ECommerceBackendSchema < GraphQL::Schema
   use GraphQL::Dataloader
 
   # GraphQL-Ruby calls this when something goes wrong while running a query:
-  def self.type_error(err, context)
-    # if err.is_a?(GraphQL::InvalidNullError)
-    #   # report to your bug tracker here
-    #   return nil
-    # end
-    super
-  end
 
   # Union and Interface Resolution
   def self.resolve_type(_abstract_type, _obj, _ctx)
--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Category < ApplicationRecord
-  has_many :product_categories, dependent: :destroy
-  has_many :products, through: :product_categories
-end

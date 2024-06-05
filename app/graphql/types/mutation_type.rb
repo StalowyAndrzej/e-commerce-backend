628444a549b20864c7bdf866c598b4b584309431
Andrzej <andrzejkar17@gmail.com>
2024-06-05 07:17:59 +0200

    Clean mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,8 +5,5 @@ module Types
     # TODO: remove me
     field :test_field, String, null: false,
                                description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
-    end
   end
 end

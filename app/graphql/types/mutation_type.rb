f9b25e40194fa28940dccc0e594604ff9bacc53e
Andrzej <andrzejkar17@gmail.com>
2024-06-06 10:41:22 +0200

    Remove descript

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
+    field :test_field, String, null: false
   end
 end

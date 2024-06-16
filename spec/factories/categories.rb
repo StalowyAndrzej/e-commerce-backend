b1f42aec7ad15e610df5450b3521de8fd832bf0c
Andrzej <andrzejkar17@gmail.com>
2024-06-16 09:59:38 +0200

    Clean up factories for tests

--- spec/factories/categories.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :category do
-    name { 'MyString' }
-    description { 'MyString' }
-    is_active { false }
-  end
-end
--- spec/factories/product_categories.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :product_category do
-    product { nil }
-    category { nil }
-  end
-end

78e06a2fe9ba0c665523a612bd973e1c2317781d
Andrzej <andrzejkar17@gmail.com>
2024-06-22 20:49:37 +0200

    Cop product type type

--- app/graphql/types/product_category_type.rb
+++ app/graphql/types/product_category_type.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module Types
   class ProductCategoryType < Types::BaseObject
-    field :id, ID, null: false
-    field :product_id, Integer, null: false
     field :category_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :id, ID, null: false
+    field :product_id, Integer, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
